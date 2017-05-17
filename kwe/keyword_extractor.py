e77987baa3bcfef255e804a7153054f5e450e42e
Pierpaolo Pantone <24alsecondo@gmail.com>
2017-05-17 17:49:36 +0200

    Add keyword extraction logic to evaluate scores
    Note: scores are only related to a specific document, and not compared with corpus documents.

--- kwe/keyword_extractor.py
+++ kwe/keyword_extractor.py
@@ -3,16 +3,72 @@
 
 """Keyword extraction module for KWE."""
 
+import itertools
+from collections import defaultdict
+
+import pandas as pd
+
 from kwe.tokenizer import KeywordTokenizer
 
 class KeywordExtractor(object):
+    """Keyword extraction class."""
 
-    def __init__(self, input_file, max_keyword_size=3):
+    def __init__(self, input_file, max_keyword_size=3, tokenizer=None):
         self.input_file = input_file
         self.max_keyword_size = max_keyword_size
+        self.co_occurrency_matrix = None
+        self.all_words = None
+
+        if tokenizer is None:
+            self.tokenizer = KeywordTokenizer
+        else:
+            self.tokenizer = tokenizer
 
     def extract_keywords(self):
+        candidate_keywords = self._extract_keyword_candidates()
+        self._build_co_occurrency_matrix(candidate_keywords)
+        self._compute_all_keyword_scores(candidate_keywords)
+
+    def _extract_keyword_candidates(self):
         sentences = KeywordTokenizer.tokenize_sentences(self.input_file)
         candidate_keywords = list(KeywordTokenizer.tokenize_keywords(
             sentences, self.max_keyword_size
         ))
+
+        return candidate_keywords
+
+    def _build_co_occurrency_matrix(self, candidate_keywords):
+        self.all_words = {word.lower() for word in itertools.chain.from_iterable(candidate_keywords)}
+        self.co_occurrency_matrix = pd.DataFrame(
+            0, index=self.all_words, columns=self.all_words
+        )
+
+        for kw in candidate_keywords:
+            for w1 in kw:
+                for w2 in kw:
+                    self.co_occurrency_matrix[w1.lower()][w2.lower()] += 1
+
+    def _compute_all_keyword_scores(self, candidate_keywords):
+        # Create a pandas Series containing word degree scores. We consider
+        # words as vertices of the co-occurrency graph. A degree of a word is
+        # therefore the sum of all its co-occurrencies.
+        word_degrees     = self.co_occurrency_matrix.sum(axis=1)
+        word_frequencies = self._compute_word_frequencies()
+        word_scores      = self._compute_word_scores(word_degrees, word_frequencies)
+
+        keyword_scores = defaultdict(float)
+        for keyword in candidate_keywords:
+            score = 0
+            for word in keyword:
+                score += word_scores[word.lower()]
+            keyword_scores[' '.join(keyword)] = score
+
+    def _compute_word_frequencies(self):
+        return {word : self.co_occurrency_matrix[word][word] for word in self.all_words}
+
+    def _compute_word_scores(self, word_degrees, word_frequencies):
+        word_scores = defaultdict(float)
+        for word in self.all_words:
+            word_scores[word] = word_degrees[word] / word_frequencies[word]
+
+        return word_scores
